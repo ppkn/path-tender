f016902848d6057c74d00fd7ca111b14b0a3f36b
Daniel Pipkin <drpipkin@gmail.com>
2024-04-20 04:03:51 -0600

    Add picocss

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import "./index.scss";
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -13,6 +15,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <link rel="icon" type="image/svg+xml" href="/compass.svg" />
         <Links />
       </head>
       <body>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,31 +2,15 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix SPA" },
-    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
+    { title: "Login" },
+    { name: "description", content: "Login to Path Tender" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (SPA Mode)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/future/spa-mode"
-            rel="noreferrer"
-          >
-            SPA Mode Guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <header>
+      <h1>Path Tender 🌱🚶🚴</h1>
+    </header>
   );
 }
