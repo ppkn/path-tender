9f4e94c7361caf2a6620d4947763cd8f81b8759b
Daniel Pipkin <drpipkin@gmail.com>
2024-04-21 11:34:45 -0600

    List entries on home view

--- app/routes/_app._index.tsx
+++ app/routes/_app._index.tsx
@@ -1,10 +1,18 @@
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import { pb } from "~/pocketbase";
 
 export const clientLoader = async () => {
-  return { user: pb.authStore.model };
+  return { entries: await pb.collection("entries").getFullList() };
 };
 export default function AppIndex() {
-  const { user } = useLoaderData<typeof clientLoader>();
-  return <>Hello {user?.name}</>;
+  const { entries } = useLoaderData<typeof clientLoader>();
+  return (
+    <ul>
+      {entries.map((entry) => (
+        <li key={entry.id}>
+          <Link to={`/entry/${entry.id}`}>{entry.created}</Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
