c8bfa6fc3255ab4f0fb78792bee6f0eea3dbf819
Daniel Pipkin <drpipkin@gmail.com>
2024-04-21 15:43:51 -0600

    Add photo upload

--- app/routes/_app.entry.new.tsx
+++ app/routes/_app.entry.new.tsx
@@ -1,14 +1,21 @@
 import { ClientActionFunctionArgs, Form, redirect } from "@remix-run/react";
+import exifr from "exifr";
 import invariant from "tiny-invariant";
 import { pb } from "~/pocketbase";
 
 export const clientAction = async ({ request }: ClientActionFunctionArgs) => {
   const formData = await request.formData();
-  const { notes, publish } = Object.fromEntries(formData);
+  const { notes, publish, photo } = Object.fromEntries(formData);
   invariant(typeof notes === "string", "notes needs to be a string");
+  invariant(photo instanceof File, "photo must be a file");
+
+  const { latitude, longitude } = await exifr.gps(photo);
   const entry = await pb.collection("entries").create({
-    notes,
     isPublished: publish == "on",
+    latitude,
+    longitude,
+    notes,
+    photo,
     user: pb.authStore.model?.id,
   });
   return redirect(`/entry/${entry.id}`);
@@ -17,7 +24,11 @@ export const clientAction = async ({ request }: ClientActionFunctionArgs) => {
 export default function NewEntry() {
   return (
     <article>
-      <Form method="post">
+      <Form encType="multipart/form-data" method="post">
+        <label>
+          Photo
+          <input type="file" name="photo" />
+        </label>
         <textarea name="notes" placeholder="Notes" />
         <label>
           <input type="checkbox" name="publish" role="switch" />
